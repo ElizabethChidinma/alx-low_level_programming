49fbfeb0a88bd42fa9bc9ddf572ce1a53a309087
ElizabethChidinma <echidinma68@gmail.com>
2023-09-07 11:37:02 -0700

    preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - A soft program that print a line using the printf f function
- * return: 0 (Good)
+ *
+ * Return: 0 (Good)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return(0);
-}
+	return (0)
