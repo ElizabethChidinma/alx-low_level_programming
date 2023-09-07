527738687773ab4492a104ddedd652364443729c
ElizabethChidinma <echidinma68@gmail.com>
2023-09-07 09:38:38 -0700

    1-compiler

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A soft program that print a line using the printf f function
+ * return: 0 (Good)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return(0);
+}
