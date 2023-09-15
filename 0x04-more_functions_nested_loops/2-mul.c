9d43873ed8d9fb48f6028ba58784fce6d8cc63fb
ElizabethChidinma <echidinma68@gmail.com>
2023-09-15 10:48:12 -0700

    git

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mu1 - multiply 2int
+ * @a: first int
+ * @b: second int
+ * Return: addiction of the multiplication
+ */
+
+int mu1(int a, int b)
+{
+	return (a * b);
+}
