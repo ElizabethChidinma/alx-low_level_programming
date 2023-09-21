e54cc231161bb3f9175159e80a9dc58e699775a6
ElizabethChidinma <echidinma68@gmail.com>
2023-09-21 02:56:12 -0700

    fuy

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,12 +12,11 @@ void rev_string(char *n)
 	int j = 0;
 	char temp;
 
-	while (*(n + i) != '0')
+	while (*(n + i) != '\0')
 	{
 		i++;
 	}
 	i--;
-
 	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
@@ -25,15 +24,15 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
+
 /**
- * infinte_add - add 2 numbers together
- * @n1: text representation of ist number to add
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
- * @size_r buffer size
+ * @size_r: buffer size
  * Return: pointer to calling function
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
@@ -43,8 +42,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		i++;
 	while (*(n2 + j) != '\0')
 		j++;
-	j--;
 	i--;
+	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
