dc17581140b2e9fa518f48a8cd10f46c21f4313d
ElizabethChidinma <echidinma68@gmail.com>
2023-12-12 10:53:39 -0800

    upd

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +24,11 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
 #endif
